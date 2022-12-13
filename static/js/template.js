89e610dc9444b33d48809661b715cd0c73c3e700
Adil Shabab <adilshabab2004@gmail.com>
2022-12-13 18:52:46 +0530

    todays work completed

--- static/js/template.js
+++ static/js/template.js
@@ -20,18 +20,6 @@
             element.addClass('active');
           }
         }
-      } else {
-        //for other url
-        if (element.attr('href').indexOf(current) !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
-          if (element.parents('.submenu-item').length) {
-            element.addClass('active');
-          }
-        }
       }
     }
 
